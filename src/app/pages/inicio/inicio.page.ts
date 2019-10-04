aba0f06e35350e8bc7bcaa41fdf69526aba2ff3b
edgar <e.torrielli@gmail.com>
2019-10-04 20:31:52 -0300

    update

--- src/app/pages/inicio/inicio.page.ts
+++ src/app/pages/inicio/inicio.page.ts
@@ -1,15 +1,35 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
-  selector: 'app-inicio',
-  templateUrl: './inicio.page.html',
-  styleUrls: ['./inicio.page.scss'],
+    selector: 'app-inicio',
+    templateUrl: './inicio.page.html',
+    styleUrls: ['./inicio.page.scss'],
 })
 export class InicioPage implements OnInit {
 
-  constructor() { }
+    componentes: Componente[] = [
+        {
+            icon: 'american-football',
+            name: 'Action Sheet',
+            redirectTo: '/action-sheet'
+        },
+        {
+            icon: 'appstore',
+            name: 'Alert',
+            redirectTo: '/alert'
+        }
+    ];
 
-  ngOnInit() {
-  }
+    constructor() {
+    }
 
+    ngOnInit() {
+    }
+
+}
+
+interface Componente {
+    icon: string;
+    name: string;
+    redirectTo: string;
 }
